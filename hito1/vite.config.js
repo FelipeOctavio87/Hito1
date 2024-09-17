4e3d373d3bf507343907a49d4416fd85edcefe4e
FelipeOctavio87 <felipe.rebolledo.robert@gmail.com>
2024-09-17 11:12:18 -0300

    vite

--- hito1/vite.config.js
+++ hito1/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Hito1/', // Reemplaza 'Hito1' con el nombre de tu repositorio
+  build: {
+    outDir: 'dist', // Asegúrate de que 'dist' sea la carpeta que estás utilizando para el despliegue
+  },
+});
